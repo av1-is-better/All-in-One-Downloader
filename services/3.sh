923f04b4cf6e6331e544a02f918ee1fe63a870cd
Kunal Yadav <kunaly@yahoo.com>
2025-04-04 17:27:14 +0530

    ttyd

--- services/3.sh
+++ services/3.sh
@@ -2,5 +2,5 @@
 # ttyd
 
 while true; do
-    /usr/bin/uG37Yq -p 61802 -u 0 -t "fontSize=22" -w "/app" -c "admin:${GLOBAL_PASSWORD}" --writable bash
+    /usr/bin/uG37Yq -p 61802 -u 0 -t "fontSize=22" -w "/app" --writable bash
 done
