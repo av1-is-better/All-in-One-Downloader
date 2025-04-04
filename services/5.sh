6d8e3fe8f4095ac4c5ff8d728b076dabc0566ba0
Kunal Yadav <kunaly@yahoo.com>
2025-04-04 17:45:02 +0530

    rclone rcd

--- services/5.sh
+++ services/5.sh
@@ -3,5 +3,8 @@
 # rclone rcd
 
 while true; do
-    /usr/bin/PKpA10 rcd --rc-web-gui --rc-htpasswd /app/config/.rclone_htpasswd --rc-addr=:61801 --buffer-size=96M --transfers=2 --drive-chunk-size=512M --checkers=2 --ignore-existing --size-only --drive-upload-cutoff=1000T --retries=999 --rc-allow-origin="127.0.0.1" --config=/app/config/rclone.conf
+    # Old Command (Explicitly Set Username and Password)
+    # /usr/bin/PKpA10 rcd --rc-web-gui --rc-htpasswd /app/config/.rclone_htpasswd --rc-addr=:61801 --buffer-size=96M --transfers=2 --drive-chunk-size=512M --checkers=2 --ignore-existing --size-only --drive-upload-cutoff=1000T --retries=999 --rc-allow-origin="127.0.0.1" --config=/app/config/rclone.conf
+    # New Command (Caddy Will Provide Security Layer)
+    /usr/bin/PKpA10 rcd --rc-web-gui --rc-no-auth --rc-addr=127.0.0.1:61801 --buffer-size=96M --transfers=2 --drive-chunk-size=512M --checkers=2 --ignore-existing --size-only --drive-upload-cutoff=1000T --retries=999 --rc-allow-origin="127.0.0.1" --config=/app/config/rclone.conf
 done
