76101edaa9872e63be5ef0804b2cebd00600c0e7
Kunal Yadav <kunaly@yahoo.com>
2025-04-07 19:07:55 +0530

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -67,7 +67,7 @@ if grep -q "GLOBALPASSWORDHERE" "$ARIA_CONFIG_FILE"; then
 fi
 
 # ariang html (replacing port and RPC token in js file)
-ARIANG_JS_FILE="/var/www/aria/js/aria-ng-b72c9fc1fc.min.js"
+ARIANG_JS_FILE="/var/www/aria/js/aria-ng-c4c1cf5975.min.js"
 UNTOUCHED_FILE="/var/www/aria/js/aria-ng-untouched.js"
 if [[ -f "$ARIANG_JS_FILE" ]]; then
     if [[ -f "$UNTOUCHED_FILE" ]]; then
